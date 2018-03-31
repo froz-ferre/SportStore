1fe4917dbdbfa39fd0bce311f471958f71dadda8
Froz-Ferre <alexej.ferre@gmail.com>
2018-03-31 13:11:52 +0300

    add store component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './src/app/store/store.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule
